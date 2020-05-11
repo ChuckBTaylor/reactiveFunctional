535667aa4355de74ddacfbf574442edcce9d5aa2
ChuckBTaylor <WLScripps@gmail.com>
2020-05-11 12:18:18 +0300

    Nested routing

--- src/main/java/com/wirebraincoffee/productapifunctional/ProductApiFunctionalApplication.java
+++ src/main/java/com/wirebraincoffee/productapifunctional/ProductApiFunctionalApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,7 +14,8 @@ import com.wirebraincoffee.productapifunctional.handler.ProductHandler;
 import com.wirebraincoffee.productapifunctional.model.Product;
 import com.wirebraincoffee.productapifunctional.repository.ProductRepository;
 
-import static org.springframework.web.reactive.function.server.RouterFunctions.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
@@ -38,12 +40,21 @@ public class ProductApiFunctionalApplication {
 
 	@Bean
 	public RouterFunction<ServerResponse> routes(ProductHandler handler) {
-		return route(GET("/products").and(accept(APPLICATION_JSON)), handler::getAllProducts)
-				.andRoute(POST("/products").and(contentType(APPLICATION_JSON)), handler::postProduct)
-				.andRoute(DELETE("/products").and(accept(APPLICATION_JSON)), handler::deleteAllProducts)
-				.andRoute(GET("/products/events").and(accept(TEXT_EVENT_STREAM)), handler::getProductEvents)
-				.andRoute(GET("/products/{id}").and(accept(APPLICATION_JSON)), handler::getProduct)
-				.andRoute(PUT("products/{id}").and(contentType(APPLICATION_JSON)), handler::updateProduct)
-				.andRoute(DELETE("/products/{id}").and(accept(APPLICATION_JSON)), handler::deleteProduct);
+	// @formatter:off
+		return nest(path("/products"),
+				nest(accept(APPLICATION_JSON).or(contentType(APPLICATION_JSON).or(accept(TEXT_EVENT_STREAM))),
+						route(method(HttpMethod.GET), handler::getAllProducts)
+						.andRoute(method(HttpMethod.POST), handler::postProduct)
+						.andRoute(method(HttpMethod.DELETE), handler::deleteAllProducts)
+						.andRoute(GET("/events"), handler::getProductEvents)
+						.andNest(path("/{id}"),
+								route(method(HttpMethod.GET), handler::getProduct)
+								.andRoute(method(HttpMethod.PUT), handler::updateProduct)
+								.andRoute(method(HttpMethod.DELETE), handler::deleteProduct)
+						)
+				)
+		);
+    // @formatter:on
+
 	}
 }
